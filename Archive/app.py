0c94453d61f999011429fcc160cafb37b1088d38
Ethan Bellmer <e.bellmer@outlook.com>
2020-03-10 12:39:10 +0000

    Remove unused app.py

--- Archive/app.py
+++
@@ -1,5 +0,0 @@
-# Venv activation is blocked by default because the process isn't singed, so run this first:
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-
-
-print ('hello world')
