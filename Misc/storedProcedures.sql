c7cc6063e81b789e798aaae83509bf346fd940ce
Ethan Bellmer <e.bellmer@outlook.com>
2020-04-06 09:11:18 +0100

    Start to write procedures to push sensor data

--- Misc/storedProcedures.sql
+++ Misc/storedProcedures.sql
@@ -1,1 +1,43 @@
-CREATE PROCEDURE PROC_INSERT_DATA():
+CREATE PROCEDURE PROC_INSERT_DATA(@sensorID AS INT, @applicationID AS INT, @networkID AS INT, @sensorName AS NVARCHAR(MAX), @dataMessageGUID AS UNIQUEIDENTIFIER, @dTypeID AS UNIQUEIDENTIFIER, @reading AS NVARCHAR(5), @messageType AS NVARCHAR(5), @signalStrength AS FLOAT, @batteryLevel AS INT, @pendingChange AS BIT, @voltage AS FLOAT)
+AS
+BEGIN
+	SELECT sensorID
+	FROM salfordMove.dbo.SENSORS
+	WHERE sensorID = @sensorID
+END;
+
+
+
+(SENSORS)
+sensorID
+applicationID
+networkID
+sensorName
+
+(READINGS)
+dataMessageGUID
+sensorID
+dTypeID
+reading / rawData
+messageType
+
+(SIGNAL_STATUS)
+sensorID
+dataMessageGUID
+signalStrength
+
+(BATTERY_STATUS)
+sensorID
+dataMessageGUID
+batteryLevel
+
+
+(PENDING_CHANGES)
+sensorID
+dataMessageGUID
+pendingChange
+
+(SENSOR_VOLTAGE)
+sensorID
+dataMessageGUID
+voltage
