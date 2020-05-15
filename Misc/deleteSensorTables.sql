47f7c7099c5064ac0295b2658ed5d76d71f48bce
Ethan Bellmer <e.bellmer@outlook.com>
2020-05-15 12:45:29 +0100

    Add PLOT_LABELS tabel

--- Misc/deleteSensorTables.sql
+++ Misc/deleteSensorTables.sql
@@ -10,6 +10,8 @@ DROP TABLE salfordMove.dbo.READINGS;
 
 DROP TABLE salfordMove.dbo.DATA_TYPES;
 
+DROP TABLE salfordMove.dbo.PLOT_LABELS;
+
 DROP TABLE salfordMove.dbo.SENSORS;
 
 DROP TABLE salfordMove.dbo.NETWORKS;
