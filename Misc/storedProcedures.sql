afaad59e9dc01a6c82c6de60f1aebeaa4f1caacb
Ethan Bellmer <e.bellmer@outlook.com>
2020-05-15 14:33:04 +0100

    Add procedures for creating network & applications

--- Misc/storedProcedures.sql
+++ Misc/storedProcedures.sql
@@ -1,3 +1,44 @@
+/* Check if NETWORK exists, if not create it */
+CREATE PROCEDURE PROC_GET_OR_CREATE_NETOWRK(@networkID INT)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF EXISTS
+	(
+		SELECT networkID
+		FROM salfordMove.dbo.NETWORKS
+		WHERE networkID = @networkID
+	)
+	PRINT('NETWORK ID EXISTS IN TABLE, SKIPPING...')
+
+	ELSE
+		INSERT INTO salfordMove.dbo.NETWORKS (networkID) VALUES (@networkID)
+END;
+GO
+
+
+/* Check if APPLICATION exists, if not create it */
+CREATE PROCEDURE PROC_GET_OR_CREATE_APPLICATION(@applicationID INT)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF EXISTS
+	(
+		SELECT applicationID
+		FROM salfordMove.dbo.APPLICATIONS
+		WHERE applicationID = @applicationID
+	)
+	PRINT('APPLICATION ID EXISTS IN TABLE, SKIPPING...')
+
+	ELSE
+		INSERT INTO salfordMove.dbo.APPLICATIONS (applicationID) VALUES (@applicationID)
+END;
+GO
+
+
+/* Check if SENSOR exists, 
+if not create it and return the generated sensorID, 
+if exists SELECT sensorID from table and return it */
 CREATE PROCEDURE PROC_GET_OR_CREATE_SENSOR (@applicationID as INT, @networkID as INT, @sensorName as NVARCHAR(20), @sensorID UNIQUEIDENTIFIER OUTPUT)
 AS
 BEGIN
@@ -17,18 +58,10 @@ BEGIN
 END;
 GO
 
-/*
-## For calling the procedure:
-DECLARE @applicationID = applicationID
-DECLARE @networkID = networkID
-DECLARE @sensorName = sensorName
-DECLARE @sensorID UNIQUEIDENTIFIER
-EXECUTE PROC_SELECT_SENSOR_ID @applicationID, @networkID, sensorID OUTPUT
-## sensorID = EXECUTE PROC_SELECT_SENSOR_ID (@applicationID, @networkID, @sensorName)
-PRINT @sensorID
-*/
 
-
+/* Check if DATA TYPE exists, 
+if not create it and return the generated dataTypeID, 
+if exists SELECT dataTypeID from table and return it */
 CREATE PROCEDURE PROC_GET_OR_CREATE_DATA_TYPE (@dataType AS NVARCHAR(20), @dataTypeID UNIQUEIDENTIFIER OUTPUT)
 AS
 BEGIN
@@ -49,16 +82,10 @@ BEGIN
 END;
 GO
 
-/*
-## For calling the procedure:
-DECLARE @dataType = dataType
-DECLARE @dataTypeID UNIQUEIDENTIFIER
-EXECUTE PROC_GET_OR_CREATE_DATA_TYPE @dataType, @dataTypeID OUTPUT
-## dataTypeID = EXECUTE PROC_SELECT_SENSOR_ID (@dataType)
-PRINT @dataTypeID
-*/
 
-
+/* Check if PLOT LABEL exists,
+if not create it and return the generated plotLabelID, 
+if exists SELECT plotLabelID from table and return it */
 CREATE PROCEDURE PROC_GET_OR_CREATE_PLOT_LABELS (@plotLabel AS NVARCHAR(20), @pLabelID UNIQUEIDENTIFIER OUTPUT)
 AS
 BEGIN
@@ -79,16 +106,9 @@ BEGIN
 END;
 GO
 
-/*
-## For calling the procedure:
-DECLARE @dataType = dataType
-DECLARE @pLabelID UNIQUEIDENTIFIER
-EXECUTE PROC_GET_OR_CREATE_PLOT_LABELS @dataType, @pLabelID OUTPUT
-## plotLabelID = EXECUTE PROC_GET_OR_CREATE_PLOT_LABELS (@dataType)
-PRINT @plotLabelID
-*/
 
-
+/* Create a new reading in the database, 
+and return the genreated readingID */
 CREATE PROCEDURE PROC_CREATE_READING (@readingID UNIQUEIDENTIFIER OUTPUT, @dataMessageGUID UNIQUEIDENTIFIER, @sensorID UNIQUEIDENTIFIER, @rawData NVARCHAR(10), @dTypeID UNIQUEIDENTIFIER, @dataValue NVARCHAR(10), @pLabelID UNIQUEIDENTIFIER, @plotValue NVARCHAR(10), @messageDate DATETIME, @messageType NVARCHAR(10))
 AS
 BEGIN
@@ -100,6 +120,7 @@ END;
 GO
 
 
+/* Create a new Signal Status entry */
 CREATE PROCEDURE PROC_CREATE_SIGNAL_STATUS (@readingID UNIQUEIDENTIFIER, @dataMessageGUID UNIQUEIDENTIFIER, @signalStrength FLOAT)
 AS
 BEGIN
@@ -109,6 +130,7 @@ END;
 GO
 
 
+/* Create a new Battery Status entry */
 CREATE PROCEDURE PROC_CREATE_BATTERY_STATUS (@readingID UNIQUEIDENTIFIER, @dataMessageGUID UNIQUEIDENTIFIER, @batteryLevel INT)
 AS
 BEGIN
@@ -118,6 +140,7 @@ END;
 GO
 
 
+/* Create a new Pending Change entry */
 CREATE PROCEDURE PROC_CREATE_PENDING_CHANGES (@readingID UNIQUEIDENTIFIER, @dataMessageGUID UNIQUEIDENTIFIER, @pendingChange BIT)
 AS
 BEGIN
@@ -127,6 +150,7 @@ END;
 GO
 
 
+/* Create a new Sensor Voltage entry */
 CREATE PROCEDURE PROC_CREATE_SENSOR_VOLTAGE (@readingID UNIQUEIDENTIFIER, @dataMessageGUID UNIQUEIDENTIFIER, @voltage FLOAT)
 AS
 BEGIN
@@ -134,107 +158,3 @@ BEGIN
 	INSERT INTO salfordMove.dbo.SENSOR_VOLTAGE (readingID, dataMessageGUID, voltage) VALUES (@readingID, @dataMessageGUID, @voltage)
 END;
 GO
-
-/*
-	How can this be done?
-	
-	- Message is recieved
-		- JSON read and converted to Pandas DF
-		- Data needs to be split to remove concatenated strings in sensor values
-
-
-		-Relevant IDs are looked up in the DB
-			Looked up ID's are 
-
-	Some values will need to be looked up in the table before they can be inserted.
-	Data type ID will always need to be looked up before insertion because it's reliant on a value in an external table and uses the ID to look up the data type value.
-		Data type table will need to be manually populated, can possobly be included in the procedures but may be difficult to implement.
-
-	sensorID needs to be looked up, and if it doesn't exist it needs to create a new sensor entry in the SENSORS table, but if it does exist if only needs to enter the sensorID into the other tables.
-
-	Application ID, Network ID, and Account ID don't need to be looked up as they're always the same value and their respective tables exist for the sake of futureproofing 
-	the system in case data relating to applicaiton, network, and account becomes available through the API at some point.
-
-
-	Sequence:
-	- Select dataTypeID from DATA_TYPES
-	- Insert sensorID, applicationID, networkID, and sensorName into SENSORS if entry doesn't already exist
-	-- If exists just insert sensorID into other tables
-
-	- Insert sensorID, dataMessageID, and signalStrength into SIGNAL_STATUS
-	- Insert sensorID, dataMessageID, and betteryLevel into BATTERY_STATUS
-	- Insert sensorID, dataMessageID, and pendingChange into PENDING_CHANGES
-
-	- Insert sensorID, dataMessageGUID, dTypeID, rawData, messageDate into READINGS
-
-
-
-	A sensor message will always have 
-
-	sensorID
-	applicationID
-	networkID
-	sensorName
-
-	dataMessageGUID
-	dTypeID
-	reading / rawData
-	messageType
-
-	(SIGNAL_STATUS)
-	sensorID
-	dataMessageGUID
-	signalStrength
-
-	(BATTERY_STATUS)
-	sensorID
-	dataMessageGUID
-	batteryLevel
-
-
-	(PENDING_CHANGES)
-	sensorID
-	dataMessageGUID
-	pendingChange
-
-	(SENSOR_VOLTAGE)
-	sensorID
-	dataMessageGUID
-	voltage
-
-
-	(SENSORS)
-	sensorID
-	applicationID
-	networkID
-	sensorName
-
-	(READINGS)
-	dataMessageGUID
-	sensorID
-	dTypeID
-	reading / rawData
-	messageType
-
-	(SIGNAL_STATUS)
-	sensorID
-	dataMessageGUID
-	signalStrength
-
-	(BATTERY_STATUS)
-	sensorID
-	dataMessageGUID
-	batteryLevel
-
-
-	(PENDING_CHANGES)
-	sensorID
-	dataMessageGUID
-	pendingChange
-
-	(SENSOR_VOLTAGE)
-	sensorID
-	dataMessageGUID
-	voltage
-
-*/
