ea6e2e44f47c86455d1ed39131eadb9ff6de3e8d
Ethan Bellmer <e.bellmer@outlook.com>
2020-08-05 11:17:42 +0100

    Split sensors into separate worksheets

--- processExistingData-toCSV.py
+++ processExistingData-toCSV.py
@@ -2,10 +2,11 @@
 # Description: Fetches the data that has already been 
 # 	received from the Monnit servers from the SQL DB 
 # 	and processes it into it's normalised form using 
-# 	the updated model. 
+# 	the updated model.
+#	Processed data is saved to CSV and XLSX.
 # Author: Ethan Bellmer
-# Date: 09/07/2020
-# Version: 1.0
+# Date: 05/08/2020
+# Version: 2.0
 
 
 # Import libraries
@@ -32,24 +33,46 @@ with open("./config/.dbCreds.json") as f:
 	dbCreds = json.load(f)
 
 CSV_DIR = os.getcwd() + '/data/csv/'
+XLSX_DIR = os.getcwd() + '/data/xlsx/'
 
 # Formatted connection string for the SQL DB.
 SQL_CONN_STR = 'DSN='+dbCreds['SERVER']+';Database='+dbCreds['DATABASE']+';Trusted_Connection=no;UID='+dbCreds['UNAME']+';PWD='+dbCreds['PWD']+';'
 
 
 ## Functions ##
+def filterNetwork(pdDF):
+	print('Filtering out unused network data')
+	pdDF = pdDF[splitDf.networkID == 58947]
+	#pdDF = pdDF.drop(columns = ['Unnamed: 0'])
 
-# Iterates through list of returned strings from the DB into and converts them to GUID
-def strToUUID(struct):
-	for element in struct:
-		# Remove the leading and trailing characters from the ID
-		element = struct.replace("[('", "")
-		element = struct.replace("', )]", "")
-		# Convert trimmed string into a GUID (UUID)
-		element = UUID(element)
-		
-	# Return the list of UUIDs
-	return struct
+	return pdDF
+
+def splitSensors(pdDF):
+	#writer = pd.ExcelWriter("sensorData.xlsx", engine = 'xlsxwriter')
+
+	print('Splitting data by sensor ID')
+
+	print('Sorting Values')
+	pdDF.sort_values(by = ['sensorID'], inplace = True)
+	
+	print('Setting Index')
+	pdDF.set_index(keys = ['sensorID'], drop = False, inplace = True)
+
+	print('Stripping Index Name')
+	pdDF.index.name = None
+
+	print('Exporting Sensor data to CSV & XLSX')
+	with pd.ExcelWriter(XLSX_DIR + 'sensorData.xlsx') as writer: # pylint: disable=abstract-class-instantiated
+		for i, x in pdDF.groupby('sensorID'):
+			print('Processing Sensor ' + str(i))
+
+			# Export the data to separate CSV files
+			p = os.path.join(CSV_DIR, "sensor_{}.csv".format(i))
+			x.to_csv(p, index=False)
+			
+			# Export the data to a single XLSX file with a worksheet per sensor 
+			x.to_excel(writer, sheet_name = str(i))
+		#writer.save
 
 
 ## Main Body ##
@@ -64,8 +87,7 @@ SQL = "SELECT * FROM salfordMove.dbo.sensorData"
 
 print('Getting data from DB')
 oldData = pd.read_sql(SQL,conn)
-#oldData = oldData.head()
-#print(oldData)
+#oldData = oldData.head(20) # Overrites the dataframe wiht the top 20 entires for quick testing
 
 # Delimeters used in the recieved data
 delimeters = "%2c","|"
@@ -87,12 +109,11 @@ for i, row in splitDf.iterrows():
 		splitDf.at[i, 'pendingChange'] = 1
 
 
-# Check if CSV exists, create if it doesn't
-if os.path.exists(CSV_DIR + 'sensorDataNormalised.csv'):
-	splitDf.to_csv('sensorDataNormalised.csv', mode = 'a', header = False)
-else:
-	splitDf.to_csv('sensorDataNormalised.csv', mode = 'w', header = True)
+# Pass the loaded dataframe into a function that will filter out any networks that don't involve MOVE
+networkFiltered = filterNetwork(splitDf)
 
-# Commit data to DB and close connection
-conn.commit()
+# Take the network filtered data and pass it into a functiuon that will split the data into separate sheets per sensor.
+splitSensors(networkFiltered)
+
+# Close DB connection
 conn.close()
