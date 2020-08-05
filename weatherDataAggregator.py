6f8f3325e2d2b79a775b032032402087e0c006fe
Ethan Bellmer <e.bellmer@outlook.com>
2020-08-05 16:51:29 +0100

    Initial commit

---
+++ weatherDataAggregator.py
@@ -0,0 +1,16 @@
+# Title: Weather Data Aggregator
+# Description: Aggregates data from the weather station on Cockcroft from the OnCall API.
+# Author: Ethan Bellmer
+# Date: 05/08/2020
+# Version: 1.0
+
+
+# Import libraries
+import pandas
+import json
+import requests
+
+# Function declarations
+
+
+# Main body
