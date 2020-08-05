b5addc692891b311c1383652c894e48d633f33ea
Ethan Bellmer <e.bellmer@outlook.com>
2020-08-05 16:51:12 +0100

    Include function from app to avoid conflicts

--- processExistingDataSlow.py
+++ processExistingDataSlow.py
@@ -3,6 +3,7 @@
 # 	received from the Monnit servers from the SQL DB 
 # 	and processes it into it's normalised form using 
 # 	the updated model. 
+#	Old but functional for use until the faster version is finished
 # Author: Ethan Bellmer
 # Date: 09/07/2020
 # Version: 1.0
@@ -22,10 +23,13 @@ from uuid import UUID
 
 # Import the main webhook file so its functions can be used. 
 import app
-from app import execProcedureNoReturn, execProcedure, dbConnect, split_dataframe_rows, strToUUID
+from app import dbConnect, split_dataframe_rows
+
+
+# Varable declarations 
 
 #SQL Server connection info
-with open("./../config/.dbCreds.json") as f:
+with open("./config/.dbCreds.json") as f:
 	dbCreds = json.load(f)
 
 
@@ -37,19 +41,87 @@ PWD = dbCreds['PWD']
 # Formatted connection string for the SQL DB.
 SQL_CONN_STR = 'DSN='+SERVER+';Database='+DATABASE+';Trusted_Connection=no;UID='+UNAME+';PWD='+PWD+';'
 
+
+# Function definitions
+
+def strToUUID(struct):
+	# Remove the leading and trailing characters from the ID
+	struct = struct.replace("[('", "")
+	struct = struct.replace("', )]", "")
+	# Convert trimmed string into a GUID (UUID)
+	strUUID =  UUID(struct)
+
+	# Return to calling function
+	return strUUID
+
+
+# Executes a Stored Procedure in the database to get or create data
+def execProcedure(conn, sql, params):
+	# Create new cursor from existing connection 
+	cursor = conn.cursor()
+
+	# Attempt to execute the stored procedure
+	try:
+		# Execute the SQL statement with the parameters prepared
+		cursor.execute(sql, params)
+		# Fetch all results for the executed statement
+		rows = cursor.fetchall()
+		while rows:
+			#print(rows)
+			return str(rows)
+			#if cursor.nextset(): # Disabled during testing, unsure if required if result will always return one result
+			#	rows = cursor.fetchall()
+			#else:
+			#	rows = None
+		# Close open database cursor
+		cursor.close()
+
+	except pyodbc.Error as e:
+		# Extract the error argument
+		sqlstate = e.args[1]
+
+		# Close cursor
+		cursor.close()
+
+		# Print error is one should occur and raise an exception
+		print("An error occurred executing stored procedure: " + sqlstate)
+
+
+# Executes a Stored Procedure in the database to create data without returning any values 
+def execProcedureNoReturn(conn, sql, params):
+	# Create new cursor from existing connection 
+	cursor = conn.cursor()
+
+	try:
+		# Execute the SQL statement with the parameters prepared
+		cursor.execute(sql, params)
+		# Close open database cursor
+		cursor.close()
+
+	except pyodbc.Error as e:
+		# Extract the error argument
+		sqlstate = e.args[1]
+
+		# Close cursor
+		cursor.close()
+
+		# Print error is one should occur and raise an exception
+		print("An error occurred executing stored procedure (noReturn): " + sqlstate)
+		print(e) # Testing
+
+
+# Main body 
+
 # Create a new DB object
 conn = dbConnect()
 # Create a new cursor from established DB connection
 cursor = conn.cursor()
 
 # Select all the data stored in the old DB form
-#SQL = "SELECT * FROM salfordMove.dbo.sensorData"
-
-# pandas.read_sql_table(table_name, con, schema=None, index_col=None, coerce_float=True, parse_dates=None, columns=None, chunksize=None)
-# 
-oldData = pd.read_sql_table('sensorData', conn, schema="dbo", index_col=None, coerce_float=True, parse_dates=['messageDate'], columns=['sensorID', 'sensorName', 'applicationID', 'networkID', 'dataMessgaeGUID', 'sensorState', 'messageDate', 'rawData', 'dataType', 'dataValue', 'plotValues', 'plotLabels', 'batteryLevel', 'signalStrength', 'pendingChange', 'voltage'], chunksize=None)
-
-print(oldData)
+SQL = "SELECT * FROM salfordMove.dbo.sensorData"
+oldData = pd.read_sql(SQL,conn)
+#oldData = oldData.head()
+#print(oldData)
 
 # Delimeters used in the recieved data
 delimeters = "%2c","|"
@@ -57,8 +129,6 @@ delimeters = "%2c","|"
 sensorColumns = ["rawData", "dataValue", "dataType", "plotValues", "plotLabels"]
 # Split the dataframe to move concatonated values to new rows
 splitDf = split_dataframe_rows(oldData, sensorColumns, delimeters)
-
-
 
 # Iterate through the fetched data, and convert it to it's normalised form
 for i, sensorData in splitDf.iterrows():
@@ -72,6 +142,33 @@ for i, sensorData in splitDf.iterrows():
 
 
 	##############
+
+	## CREATE NETWORK ##
+	# Prepare SQL statement to call stored procedure to create a network entry using 
+	# the networkID from the JSON
+	sql = "{CALL [dbo].[PROC_GET_OR_CREATE_NETWORK] (?)}"
+	# Bind the parameters that are required for the procedure to function
+	params = (sensorData['networkID'])
+
+	# Execute the stored procedure to create a network if it doesn't exist, 
+	# and ignore input if exists
+	#print('Step 1/10: Creating network entry')
+	execProcedureNoReturn(conn, sql, params)
+	#print('Network entry created')
+
+
+	## CREATE APPLICATION ##
+	# Prepare SQL statement to call stored procedure to create an application entry using 
+	# the applicationID from the JSON
+	sql = "{CALL [dbo].[PROC_GET_OR_CREATE_APPLICATION] (?)}"
+	# Bind the applicationID used to check if app exists in DB
+	params = (sensorData['applicationID'])
+
+	# Execute the stored procedure to create an application if it doesn't exist, 
+	# and ignore input if exists
+	#print('Step 2/10: Creating application entry')
+	execProcedureNoReturn(conn, sql, params)
+	#print('Network application created')
 
 	## GET OR CREATE SENSOR ##
 	# pyodbc doesn't support the ".callproc" function from ODBC, 
@@ -89,10 +186,10 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new sensor in the DB, or get an existing one.
-	print('Step 3/10: Creating or getting sensor')
+	#print('Step 3/10: Creating or getting sensor')
 	# Execute the procedure and return sensorID and convert trimmed string into a GUID (UUID)
 	sensorData['sensorID'] = strToUUID(execProcedure(conn, sql, params))
-	print(sensorData['sensorID'])
+	#print(sensorData['sensorID'])
 	
 
 	## GET OR CREATE DATA TYPE ##
@@ -108,9 +205,9 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new sensor in the DB, or get an existing one.
-	print('Step 4/10: Creating or getting data type ID')
+	#print('Step 4/10: Creating or getting data type ID')
 	sensorData['dataTypeID'] = strToUUID(execProcedure(conn, sql, params))
-	print(sensorData['dataTypeID'])
+	#print(sensorData['dataTypeID'])
 
 
 	## GET OR CREATE PLOT LABELS ##
@@ -125,7 +222,7 @@ for i, sensorData in splitDf.iterrows():
 	params = sensorData['plotLabels']
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new plot label in the DB, or get an existing one.
-	print('Step 5/10: Creating or getting plot label ID')
+	#print('Step 5/10: Creating or getting plot label ID')
 	sensorData['plotLabelID'] = strToUUID(execProcedure(conn, sql, params)) ## Problem here?
 	
 
@@ -143,7 +240,7 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new reading in the DB, and return the genreated ID.
-	print('Step 6/10: Creating reading, and getting ID')
+	#print('Step 6/10: Creating reading, and getting ID')
 	sensorData['readingID'] = strToUUID(execProcedure(conn, sql, params))
 	
 
@@ -157,7 +254,7 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new signal status in the DB.
-	print('Step 7/10: Creating signal atatus')
+	#print('Step 7/10: Creating signal atatus')
 	execProcedureNoReturn(conn, sql, params)
 	
 
@@ -170,7 +267,7 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new battery status in the DB.
-	print('Step 8/10: Creating battery status')
+	#print('Step 8/10: Creating battery status')
 	execProcedureNoReturn(conn, sql, params)
 	
 
@@ -184,7 +281,7 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new pending change in the DB.
-	print('Step 9/10: Creating pending change')
+	#print('Step 9/10: Creating pending change')
 	execProcedureNoReturn(conn, sql, params)
 	
 
@@ -198,5 +295,5 @@ for i, sensorData in splitDf.iterrows():
 	
 	# Execute the procedure using the prepared SQL & parameters to 
 	# create a new voltage entry in the DB.
-	print('Step 10/10: Creating voltage reading')
+	#print('Step 10/10: Creating voltage reading')
 	execProcedureNoReturn(conn, sql, params)
