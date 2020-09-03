6024a3102e257cb47e397e528b5ffe562e9a12dc
Ethan Bellmer <e.bellmer@outlook.com>
2020-09-03 15:18:39 +0100

    Perform pivot after df split

--- processExistingData-toCSV.py
+++ processExistingData-toCSV.py
@@ -13,6 +13,7 @@
 import sys
 import json
 import pandas as pd
+import numpy as np
 import os
 import traceback
 import datetime
@@ -23,7 +24,7 @@ from uuid import UUID
 
 # Import the main webhook file so its functions can be used. 
 import app
-from app import dbConnect, split_dataframe_rows
+from app import dbConnect, split_dataframe_rows, rmTrailingValues, filterNetwork
 
 
 # Variables
@@ -31,11 +32,17 @@ from app import dbConnect, split_dataframe_rows
 with open("./config/.dbCreds.json") as f:
 	dbCreds = json.load(f)
 
+# Formatted connection string for the SQL DB.
+SQL_CONN_STR = 'DSN='+dbCreds['SERVER']+';Database='+dbCreds['DATABASE']+';Trusted_Connection=no;UID='+dbCreds['UNAME']+';PWD='+dbCreds['PWD']+';'
+
+# Directory to save CSV and XLSX files to.
 CSV_DIR = os.getcwd() + '/data/csv/'
 XLSX_DIR = os.getcwd() + '/data/xlsx/'
 
-# Formatted connection string for the SQL DB.
-SQL_CONN_STR = 'DSN='+dbCreds['SERVER']+';Database='+dbCreds['DATABASE']+';Trusted_Connection=no;UID='+dbCreds['UNAME']+';PWD='+dbCreds['PWD']+';'
+pivotValues = ['rawData', 'dataValue', 'plotValues']
+pivotIndex = ['sensorID']
+pivotColumns = ['dataType', 'plotLabels']
+pivotAggFunc = "np.sum"
 
 
 # Functions
@@ -43,36 +50,41 @@ SQL_CONN_STR = 'DSN='+dbCreds['SERVER']+';Database='+dbCreds['DATABASE']+';Trust
 def filterNetwork(pdDF):
 	print('Filtering out unused network data')
 	pdDF = pdDF[splitDf.networkID == 58947]
-
 	return pdDF
-
 
 # Split the data by sensor ID and export the data to separate CSV 
 # 	files and an XLSX file with separate worksheets per sensor
-def splitSensors(pdDF):
-	print('Splitting data by sensor ID')
+def sortSensors(df):
+	print('Sorting and cleaning sensors')
+	# Sort the values in the dataframe by their sensor ID
+	df.sort_values(by = ['sensorID'], inplace = True)
+	# Set the DF index to the sensor IDs
+	df.set_index(keys = ['sensorID'], drop = False, inplace = True)
+	# Remove existing index names
+	df.index.name = None
+	return df
 
-	print('Sorting Values')
-	pdDF.sort_values(by = ['sensorID'], inplace = True)
-	
-	print('Setting Index')
-	pdDF.set_index(keys = ['sensorID'], drop = False, inplace = True)
+# Pivot passed in DF to make analysis easier. 
+# 'values', 'index', and 'columns', are all lists of variables
+def pivotTable(df, values, index, columns, aggFunc):
+	# convert the pulled data to a pivot table to make analysis easier
+	#df = pd.pivot_table(df, values=['rawData', 'dataValue', 'plotValues'], index=['sensorID'], columns=['dataType', 'plotLabels'], aggfunc=np.sum)
+	df = pd.pivot_table(df, values=values, index=index, columns=columns, aggfunc=aggFunc)
+	return df 
 
-	print('Stripping Index Name')
-	pdDF.index.name = None
-
-	print('Exporting Sensor data to CSV & XLSX')
-	with pd.ExcelWriter(XLSX_DIR + 'sensorData.xlsx') as writer: # pylint: disable=abstract-class-instantiated
-		for i, x in pdDF.groupby('sensorID'):
-			print('Processing Sensor ' + str(i))
-
-			# Export the data to separate CSV files
-			p = os.path.join(CSV_DIR, "sensor_{}.csv".format(i))
-			x.to_csv(p, index=False)
-			
-			# Export the data to a single XLSX file with a worksheet per sensor 
-			x.to_excel(writer, sheet_name = str(i))
+# Export passed in DF to XLSX files
+def toXLSX(df, sensorID):
+	print('Exporting Sensor data to XLSX')
+	with pd.ExcelWriter(XLSX_DIR + 'sensorDataSplitPivot.xlsx', mode='a') as writer: # pylint: disable=abstract-class-instantiated
+		# Export the data to a single XLSX file with a worksheet per sensor 
+		df.to_excel(writer, sheet_name = str(sensorID))
 		#writer.save
+
+# Export the data to separate CSV files
+def toCSV(df, sensorID):
+	print('Exporting Sensor data to CSV')
+	p = os.path.join(CSV_DIR, "sensor_{}.csv".format(sensorID))
+	df.to_csv(p, index=False)
 
 
 # Main Body
@@ -82,11 +94,10 @@ conn = dbConnect()
 cursor = conn.cursor()
 
 # Select all the data stored in the old DB form
-SQL = "SELECT * FROM salfordMove.dbo.sensorData"
+SQL = "SELECT TOP(200) * FROM salfordMove.dbo.sensorData"
 
 print('Getting data from DB')
 oldData = pd.read_sql(SQL,conn)
-#oldData = oldData.head(20) # Overrites the dataframe wiht the top 20 entires for quick testing
 
 # Delimeters used in the recieved data
 delimeters = "%2c","|","%7c"
@@ -97,22 +108,24 @@ print('Splitting DataFrame')
 splitDf = split_dataframe_rows(oldData, sensorColumns, delimeters)
 
 
-# Iterate through the fetched data, and convert it to it's normalised form
-for i, row in splitDf.iterrows():
-	print("Processing sensor message " + str(i) + ".")
-
-	# Convert pendingChange True/False value into a boolean 1/0
-	if row['pendingChange'] == 'False':
-		splitDf.at[i, 'pendingChange'] = 0
-	else:
-		splitDf.at[i, 'pendingChange'] = 1
+# Use the Pandas 'loc' function to find and replace pending changes in the dataset
+print('Converting Pending Changes')
+splitDf.loc[(splitDf.pendingChange == 'False'), 'pendingChange'] = 0
+splitDf.loc[(splitDf.pendingChange == 'True'), 'pendingChange'] = 1
 
 
-# Pass the loaded dataframe into a function that will filter out any networks that don't involve MOVE
-networkFiltered = filterNetwork(splitDf)
+# Split the dataframe into separate dataframes by sensorID
+for i, x in splitDf.groupby('sensorID'):
+	# Pass the loaded dataframe into a function that will filter out any networks that don't involve MOVE
+	filteredDF = filterNetwork(x)
+	# Sort the dataframe by sensorID and remove index names
+	filteredDF = sortSensors(filteredDF)
+	# Convert the dataframe to a pivot table 
+	filteredDF = pivotTable(filteredDF, pivotValues, pivotIndex, pivotColumns, pivotAggFunc)
 
-# Take the network filtered data and pass it into a functiuon that will split the data into separate sheets per sensor.
-splitSensors(networkFiltered)
+	# Export the processed data
+	toXLSX(filteredDF, i)
+	toCSV(filteredDF, i)
 
 # Close DB connection
 conn.close()
